bc7023b5930265e15efa5e54c31337f4dfbd4387
Tushar Karkera <tusharkarkera1610@gmail.com>
2014-09-10 18:17:02 -0700

    cmpe273 lab1

---
+++ HelloScala/src/main/scala/hello/HelloController.scala
@@ -0,0 +1,25 @@
+package hello
+
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController; 
+
+/**
+ * This config class will trigger Spring @annotation scanning and auto configure Spring context.
+ *
+ * @author saung
+ * @since 1.0
+ */
+
+@RestController
+class HelloController{
+
+@RequestMapping (value =Array("/")) 
+def ctl(): String={
+"hello"
+}
+
+
+
+}
